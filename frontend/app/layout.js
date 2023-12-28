b58760bffcde3e6de02e94aa50bbd3ce5450847d
unknown <anujkk19111997@gmail.com>
2023-12-28 21:34:45 +0530

    #1.02 add website nabvbar and change some resopsive for on mobile

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/ui/header'
 import Footer from '@/components/ui/footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'],weight:['400'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Header />
         {children}
         <Footer />
